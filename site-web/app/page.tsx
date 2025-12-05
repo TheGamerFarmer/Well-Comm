0996c4d0bac97d8e778f1549e1d132ce1fd199d1
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-12-05 16:21:26 +0100

    startProject

---
+++ site-web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <main className="p-8">
+          <h1 className="text-4xl font-bold text-center">Bienvenue sur le prototype !</h1>
+      </main>
+  );
+}
