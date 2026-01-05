e246e6cb460509eb5f5cd8c7459a394f369c6048
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-05 11:34:24 +0100

    add

---
+++ backend/WellComm-test/src/test/java/fr/wellcomm/WellComm/WellCommApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wellcomm.WellComm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellCommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/WellCommApplication.java
@@ -0,0 +1,13 @@
+package fr.wellcomm.wellcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellCommApplication.class, args);
+    }
+
+}
