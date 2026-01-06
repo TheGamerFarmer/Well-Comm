68342c37f61433a7236180dd2e1741af02fccdd5
TheGamerFarmer <raphaelmatheret@gmail.com>
2026-01-06 09:05:01 +0100

    Add CORS bypass to Main Class

--- backend/WellComm/src/main/java/fr/wellcomm/wellcomm/WellCommApplication.java
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/WellCommApplication.java
@@ -1,13 +1,28 @@
 package fr.wellcomm.wellcomm;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WellCommApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WellCommApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
