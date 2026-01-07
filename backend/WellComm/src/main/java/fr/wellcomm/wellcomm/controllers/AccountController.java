bbda969c00e0b40c9a0bb20e70af49ad4b25bd43
romain PLANQUETTE <romain.plqt@gmail.com>
2026-01-07 15:45:54 +0100

    add deleteRecordAccount and addRecordAccount

--- backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/AccountController.java
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/AccountController.java
@@ -1,17 +1,17 @@
 package fr.wellcomm.wellcomm.controllers;
 
 import fr.wellcomm.wellcomm.entities.Account;
+import fr.wellcomm.wellcomm.entities.Record;
+import fr.wellcomm.wellcomm.entities.RecordAccount;
 import fr.wellcomm.wellcomm.services.AccountService;
+import fr.wellcomm.wellcomm.services.RecordService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/{userName}")
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     public final AccountService accountService;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final RecordService recordService;
 
     @Getter
     @Setter
@@ -26,6 +27,19 @@ public class AccountController {
     public static class UserInfos {
         private String firstName;
         private String lastName;
+    }
+
+    @Getter
+    @Setter
+    public static class addRecordAccountRequest {
+        private long recordId;
+        private String title;
+    }
+
+    @Getter
+    @Setter
+    public static class deleteRecordAccountRequest {
+        private long recordId;
     }
 
     @GetMapping("/infos")
@@ -65,6 +79,46 @@ public class AccountController {
             return ResponseEntity.badRequest().body("User not found");
 
         accountService.deleteUser(account);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/addAccess")
+    @PreAuthorize("#userName == authentication.name")
+    public ResponseEntity<?> addRecordAccount(@PathVariable String userName, @RequestBody addRecordAccountRequest request) {
+        Account account = accountService.getUser(userName);
+        if (account == null)
+            return ResponseEntity.badRequest().body("User not found");
+
+        Record record = recordService.getRecord(request.getRecordId());
+        if (record == null)
+            return ResponseEntity.badRequest().body("Record not found");
+
+        RecordAccount newAccess = new RecordAccount();
+        newAccess.setRecord(record);
+        newAccess.setTitle(request.getTitle());
+
+        accountService.addRecordAccount(account, newAccess);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/deleteAccess/")
+    @PreAuthorize("#userName == authentication.name")
+    public ResponseEntity<?> deleteRecordAccount(@PathVariable String userName, @RequestBody deleteRecordAccountRequest request) {
+        Account account = accountService.getUser(userName);
+        if (account == null)
+            return ResponseEntity.badRequest().body("User not found");
+
+        RecordAccount recordAccount = account.getRecordAccounts().stream()
+                .filter(ra -> ra.getRecord().getId() == request.getRecordId())
+                .findFirst()
+                .orElse(null);
+
+        if (recordAccount == null)
+            return ResponseEntity.badRequest().body("Access not found");
+
+        accountService.deleteRecordAccount(account, recordAccount);
 
         return ResponseEntity.ok().build();
     }
