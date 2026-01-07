343f959c25e690c4b6689882d57ea72670422c1b
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-07 11:35:29 +0100

    Début du profil de l'aidé

---
+++ site-web/app/ProfilAide/page.tsx
@@ -0,0 +1,34 @@
+
+export default function ProfilAide() {
+    return (
+
+        <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+            <p className="text-lg font-medium font-bold text-blue-800">L'aidé</p>
+            <div>
+        <div className="w-[1095px] h-[66px] mb-[22px] pt-4 pr-[843px] pb-4 pl-[23px] bg-gradient-to-r from-blue-400 to-blue-700">
+            <p className="w-[229px] h-[34px] font-montserrat text-xl font-bold leading-[1.7px] text-left text-[#fff]">
+                DOSSIER PATIENT
+            </p>
+        </div>
+            <form className="mx-auto max-w-md">
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Prénom</label>
+                <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
+
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Nom</label>
+                <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
+
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Age</label>
+                <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
+
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Numéro de téléphone</label>
+                <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
+
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272] text-blue-600">L'aidé Info</label>
+                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Informations complémentaires</label>
+                <textarea className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"></textarea>
+
+            </form>
+        </div>
+        </div>
+    );
+}
