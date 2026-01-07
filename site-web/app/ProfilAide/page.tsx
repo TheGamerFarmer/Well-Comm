cc153b2f2f9a3845405f0d5b686b51c4b8da8958
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-07 15:16:11 +0100

    profil de l'aidé adapté au mobile

--- site-web/app/ProfilAide/page.tsx
+++ site-web/app/ProfilAide/page.tsx
@@ -6,13 +6,13 @@ export default function ProfilAide() {
         <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
             <p className="text-lg font-medium font-bold text-blue-800">L'aidé</p>
             <div>
-        <div className="w-full h-[66px] mb-[22px] pt-4 pr-[843px] pb-4 pl-[23px] bg-gradient-to-r from-[#45bbb1] to-[#215a9e]">
+        <div className="w-full h-[66px] mb-[22px] pt-4 pb-4 pl-[23px] bg-gradient-to-r from-[#45bbb1] to-[#215a9e]">
             <p className="w-[229px] h-[34px] font-montserrat text-xl font-bold leading-[1.7px] text-left text-[#fff]">
                 DOSSIER PATIENT
             </p>
         </div>
             <form className="mx-auto max-w-132">
-                <div className="flex gap-4">
+                <div className="flex flex-col md:flex-row gap-4">
                     <div>
                         <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Prénom</label>
                         <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
@@ -23,7 +23,7 @@ export default function ProfilAide() {
                     </div>
                 </div>
 
-                    <div className="flex gap-4">
+                    <div className="flex flex-col md:flex-row  gap-4">
                         <div>
                             <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Age</label>
                             <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
