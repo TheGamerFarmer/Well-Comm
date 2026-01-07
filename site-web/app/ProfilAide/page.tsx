30f1ae0d1f76c2538ad798f88d1419e5dd3e8e05
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-07 16:15:15 +0100

    profil de l'aidé petit changement

--- site-web/app/ProfilAide/page.tsx
+++ site-web/app/ProfilAide/page.tsx
@@ -4,7 +4,7 @@ export default function ProfilAide() {
     return (
 
         <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-            <p className="text-lg font-medium font-bold text-blue-800">L'aidé</p>
+            <p className="text-lg font-bold text-blue-800">L'aidé</p>
             <div>
         <div className="w-full h-[66px] mb-[22px] pt-4 pb-4 pl-[23px] bg-gradient-to-r from-[#45bbb1] to-[#215a9e]">
             <p className="w-[229px] h-[34px] font-montserrat text-xl font-bold leading-[1.7px] text-left text-[#fff]">
@@ -25,26 +25,26 @@ export default function ProfilAide() {
 
                     <div className="flex flex-col md:flex-row  gap-4">
                         <div>
-                            <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Age</label>
+                            <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Âge</label>
                             <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
                         </div>
                         <div>
-                            <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Numero de téléphone</label>
+                            <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Numéro de téléphone</label>
                             <input className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black" type="text"/>
                         </div>
                     </div>
                 <div className="w-full">
-                <label className="flex font-montserrat text-sm font-bold text-left text-[#727272] text-blue-600">L'aidé Info</label>
+                    {/*<label className="flex font-montserrat text-sm font-bold text-left text-[#727272] text-blue-600">Information de l'aidé</label>*/}
                 <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Informations complémentaires</label>
                 <textarea className="h-[50px] w-full self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"></textarea>
                 </div>
             </form>
                 <div className="flex gap-4 justify-end">
                 <Button variant="" type="submit">
-                    Cancel
+                    Annuler
                 </Button>
                 <Button type="submit">
-                    Save
+                    Enregistrer
                 </Button>
                 </div>
         </div>
