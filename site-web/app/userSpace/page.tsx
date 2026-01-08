be8a35ffb8f6d678813ac780d9a0f6177ff48ef8
mieuphoriya <ledikoska12@gmail.com>
2026-01-08 10:58:03 +0100

    ajout: Menu responsive important: installer le module de menu npm install react-burger-menu --save
    Signed-off-by: mieuphoriya <ledikoska12@gmail.com>

--- site-web/app/userSpace/page.tsx
+++ site-web/app/userSpace/page.tsx
@@ -1,9 +1,5 @@
 
-import NavigationMenu from "@/components/BurgerScreen";
-import BurgerScreen from "@/components/BurgerScreen";
-import Header from "@/components/Header";
 import HeaderLoged from "@/components/HeaderLoged";
-
 
 export default function Page() {
     return (
