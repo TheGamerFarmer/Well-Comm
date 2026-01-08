f3858f32d6d65239f284a3ea071d24b42ab72aff
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-08 11:51:30 +0100

    Components pour le tri des catégories (seulement l'interface, pas opérationnel)

---
+++ site-web/components/Categories.tsx
@@ -0,0 +1,26 @@
+import {Button} from "@/components/ButtonMain";
+
+export default function Categories(){
+    return (
+        <div className=" w-full grid grid-cols-2 sm:flex sm:flex-row flex-wrap items-center justify-center gap-3 sm:gap-5 bg-[#f3f3f3] border border-[#f3f3f3] rounded-xl p-3">
+            <Button variant="secondary">
+                Santé
+            </Button>
+            <Button variant="secondary">
+                Ménage
+            </Button>
+            <Button variant="secondary">
+                Alimentation
+            </Button>
+            <Button variant="secondary">
+                Maison/terrain
+            </Button>
+            <Button variant="secondary">
+                Hygiène
+            </Button>
+            <Button variant="secondary">
+                Autres
+            </Button>
+        </div>
+    );
+}
