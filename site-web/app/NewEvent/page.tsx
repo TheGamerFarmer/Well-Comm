2aa76c4ecfc958fbaeb5553b1e6725e578891718
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-09 10:09:59 +0100

    Centrer les titres

--- site-web/app/NewEvent/page.tsx
+++ site-web/app/NewEvent/page.tsx
@@ -5,10 +5,8 @@ export default function NewEvent() {
 
         <div className="flex justify-center items-center">
             <div className="bg-white w-[95%] rounded-xl">
-                <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-                    <p className="text-lg font-bold text-blue-800">Créer un évènement</p>
-                </div>
                 <form className="mx-auto max-w-132">
+                    <label className="text-xl font-bold text-blue-800 mb-2 flex font-montserrat  font-bold text-left text-[#727272]">Créer un évènement</label>
                     <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Titre de l'évènement</label>
                     <input type="text" className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"/>
                     <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Lieu de l'évènement</label>
--- site-web/app/NewFil/page.tsx
+++ site-web/app/NewFil/page.tsx
@@ -5,12 +5,10 @@ export default function NewFil() {
 
         <div className="flex justify-center items-center">
             <div className="bg-white w-[95%] rounded-xl">
-                <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-                    <p className="text-lg font-bold text-blue-800">Créer un Fil de transmission</p>
-                </div>
                 <form className="mx-auto max-w-132">
-                    <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Sélectionner une catégorie</label>
-                    <div className="relative inline-block w-64">
+                    <label className="text-xl font-bold text-blue-800 mb-2 flex font-montserrat  font-bold text-left text-[#727272]">Créer un Fil de transmission</label>
+                    <label className="mb-2 flex font-montserrat text-sm font-bold text-left text-[#727272]">Sélectionner une catégorie</label>
+                    <div className="mb-2 relative inline-block w-64">
                         <select
                             className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
                             <option value="1">Santé</option>
@@ -21,10 +19,10 @@ export default function NewFil() {
                             <option value="6">Autres</option>
                         </select>
                     </div>
-                    <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Sujet du fil</label>
-                    <input type="text" className="h-[50px] self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"/>
-                    <label className="flex font-montserrat text-sm font-bold text-left text-[#727272]">Description du fil</label>
-                    <textarea className="h-[100px] w-full self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"></textarea>
+                    <label className="mb-2 flex font-montserrat text-sm font-bold text-left text-[#727272]">Sujet du fil</label>
+                    <input type="text" className="h-[50px] mb-2 self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"/>
+                    <label className="mb-2 flex font-montserrat text-sm font-bold text-left text-[#727272]">Description du fil</label>
+                    <textarea className="h-[100px] w-full mb-2 self-stretch flex flex flex-row justify-between items-start py-[14px] ph-4 rounded-lg border #dfdfdf border-solid bg-[#fff]h-10 rounded-lg border-2 border-[#dfdfdf] mb-4 mt-1 p-3 text-black"></textarea>
                     <div className="flex gap-4 justify-between mb-4">
                         <Button variant="">
                             Annuler
