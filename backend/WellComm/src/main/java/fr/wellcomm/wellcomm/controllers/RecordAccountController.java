623b25c2ea7b24e7743abbbcb76b623392535bfb
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-12 15:41:36 +0100

    Création d'un recordaccount lors de la création d'un record

--- backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordAccountController.java
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordAccountController.java
@@ -1,11 +1,21 @@
 package fr.wellcomm.wellcomm.controllers;
 
+import fr.wellcomm.wellcomm.entities.Record;
 import fr.wellcomm.wellcomm.repositories.RecordAccountRepository;
+import fr.wellcomm.wellcomm.services.RecordAccountService;
+import fr.wellcomm.wellcomm.services.RecordService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/{userName}/recordsaccount")
 @AllArgsConstructor
 public class RecordAccountController {
-    private final RecordAccountRepository repository;
+    private final RecordAccountService recordAccountService;
+
 }
--- backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordController.java
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordController.java
@@ -1,9 +1,11 @@
 package fr.wellcomm.wellcomm.controllers;
 
 import fr.wellcomm.wellcomm.domain.Category;
+import fr.wellcomm.wellcomm.domain.Role;
 import fr.wellcomm.wellcomm.entities.*;
 import fr.wellcomm.wellcomm.entities.Record;
 import fr.wellcomm.wellcomm.services.AccountService;
+import fr.wellcomm.wellcomm.services.RecordAccountService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,7 @@ import fr.wellcomm.wellcomm.services.RecordService;
 public class RecordController {
     private final RecordService recordService;
     private final AccountService accountService;
+    private final RecordAccountService recordAccountService;
 
     @Getter
     @Setter
@@ -62,11 +65,14 @@ public class RecordController {
         return ResponseEntity.ok(dossiers);
     }
 
-    @GetMapping("/create/{name}")
+    @PostMapping("/create/{name}")
     @PreAuthorize("#userName == authentication.name")
     public ResponseEntity<Record> createRecord(@PathVariable @SuppressWarnings("unused") String userName,
                                                @PathVariable String name) {
+
         Record newRecord = recordService.createRecord(name);
+        Role aide = Role.AIDANT;
+        RecordAccount newRecordAccount = recordAccountService.createReccordAccount(accountService.getUser(userName), newRecord, aide);
         return ResponseEntity.ok(newRecord);
     }
 
