46cb8ef1326cb5ec8b78fae489a04cc88ea22d23
mieuphoriya <ledikoska12@gmail.com>
2026-01-12 11:47:45 +0100

    user profil
    Signed-off-by: mieuphoriya <ledikoska12@gmail.com>

--- site-web/app/userSpace/page.tsx
+++ site-web/app/userSpace/page.tsx
@@ -1,13 +1,37 @@
 
 import HeaderLoged from "@/components/HeaderLoged";
 import {Button} from "@/components/ButtonMain";
+import Image from "next/image";
 
 export default function UserSpace() {
     return (
 
 <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
             <p className=" p-4 text-lg font-bold text-blue-800">Mon Profil</p>
+
+
             <div  className=" flex justify-center items-center flex-col  bg-[#ffffff] w-[100%] rounded-xl border-20 border-white">
+
+            <div className="p-4 w-[130px]  h-[130px]">
+                <Image
+                    src="/images/avatar.svg"
+                    alt="user avatar"
+                    className="relative"
+                    width={130}
+                    height={130}
+                    priority
+                />
+
+                <Image
+                    src="/images/add_photo.svg"
+                    alt="user avatar"
+                    width={32}
+                    height={32}
+                    className="sticky right-5"
+                    priority
+                />
+
+            </div>
 
             <form className="mx-auto max-w-200 ">
                 <div className="flex flex-col md:flex-row md:gap-4">
