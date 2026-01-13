2608edc2bdfe994d822ab0fcb5cb2d6c0fdbf2f6
FeliF0x <azrextv@gmail.com>
2026-01-13 14:12:01 +0100

    Les boutons fonctionnent avec OU sans link

--- site-web/components/ButtonMain.tsx
+++ site-web/components/ButtonMain.tsx
@@ -9,7 +9,7 @@ type ButtonProps = {
     variant?: "primary" | "secondary" | "start" | "start1" | "categoryTab" | "validate" | "cancel";
     type?: "button" | "submit" | "reset";
     disabled?: boolean;
-    onClickAction?: () => void;
+    onClick?: () => void;
     link: string;
 };
 
@@ -26,19 +26,19 @@ const variantStyles = {
     start:
         "w-40 h-12 bg-[#f87c7c] text-white",
     start1:
-    "w-40 h-12 w-[shadow-[0 4px 0 0 #0551ab] bg-[#f87c7c]\n" +
+        "w-40 h-12 w-[shadow-[0 4px 0 0 #0551ab] bg-[#f87c7c]\n" +
         "\n" +
         "                    text-white py-2 px-4 md:py-4 md:px-10\n" +
         "                     shadow-[0px_4px_0px_0px_#0551ab] md:shadow-[0px_6px_0px_0px_#0551ab] text-[12px] " +
         "sm:text-sm md:text-base transition-all duration-300 hover:translate-y-[2px]",
 
     categoryTab:
-    "w-100% h-100%  py-[8px] px-2 bg-[#20baa7] " +
+        "w-100% h-100%  py-[8px] px-2 bg-[#20baa7] " +
         "hover:bg-[#20baa7] hover:text-white "+
-    "bg-white text-[#20baa7] border #20baa7 border-solid text-sm",
+        "bg-white text-[#20baa7] border #20baa7 border-solid text-sm",
 
     validate:
-       "w-100% h-100% px-4 py-[12px] rounded-[100px] bg-[#0551ab]" +
+        "w-100% h-100% px-4 py-[12px] rounded-[100px] bg-[#0551ab]" +
         " font-bold font-stretch-normal hover:bg-[#f87c7c]",
 
     cancel: "w-100% h-12 bg-[#ffffff] text-[#f87c7c] border #f87c7c border-solid  " +
@@ -51,19 +51,37 @@ export const Button = ({
                            variant = "primary",
                            type = "button",
                            disabled = false,
-                           onClickAction,
+                           onClick,
                            link,
                        }: ButtonProps) => {
+
+    const className = `${baseStyles} ${variantStyles[variant]}`;
+
+    //Si le bouton comporte un link
+    if (link){
+        return (
+            <Link href={link}>
+                <button
+                    type={type}
+                    disabled={disabled}
+                    onClick={onClick}
+                    className={`${baseStyles} ${variantStyles[variant]}`}
+                >
+                    {children}
+                </button>
+            </Link>
+        );
+    }
+    //Si le bouton ne comporte pas de link
     return (
-        <Link href={link}>
-            <button
-                type={type}
-                disabled={disabled}
-                onClick={onClickAction}
-                className={`${baseStyles} ${variantStyles[variant]}`}
-            >
-                {children}
-            </button>
-        </Link>
+        <button
+            type={type}
+            disabled={disabled}
+            onClick={onClick}
+            className={className}
+        >
+            {children}
+        </button>
     );
+
 };
