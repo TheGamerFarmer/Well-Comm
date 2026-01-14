66d6ade7452681bb95630c5eaf581ae27bbe441d
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-14 14:39:44 +0100

    Annulation du changement de response.entity dans createRecord

--- backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordController.java
+++ backend/WellComm/src/main/java/fr/wellcomm/wellcomm/controllers/RecordController.java
@@ -67,13 +67,13 @@ public class RecordController {
 
     @PostMapping("/create/{name}")
     @PreAuthorize("#userName == authentication.name")
-    public Record createRecord(@PathVariable @SuppressWarnings("unused") String userName,
+    public ResponseEntity<Record> createRecord(@PathVariable @SuppressWarnings("unused") String userName,
                                                @PathVariable String name) {
 
         Record newRecord = recordService.createRecord(name);
         Role aide = Role.AIDANT;
-        recordAccountService.createReccordAccount(accountService.getUser(userName), newRecord, aide);
-        return newRecord;
+        RecordAccount newRecordAccount = recordAccountService.createReccordAccount(accountService.getUser(userName), newRecord, aide);
+        return ResponseEntity.ok(newRecord);
     }
 
     @GetMapping("/{recordId}/channels/{category}")
