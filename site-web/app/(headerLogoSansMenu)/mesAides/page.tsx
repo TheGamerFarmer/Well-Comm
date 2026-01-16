be21327fafa23c00f5ac890f8860a523243841f0
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2026-01-16 15:57:18 +0100

    clic sur dossier envoir sur fil, register envoie sur mesAides

--- site-web/app/(headerLogoSansMenu)/mesAides/page.tsx
+++ site-web/app/(headerLogoSansMenu)/mesAides/page.tsx
@@ -148,6 +148,7 @@ export default function MesAides() {
                     {dossiers.map((dossier) =>{
                         return(
 
+                            <Link key={dossier.id} href={`/fil`}>
                         <div
                             key={dossier.id}
                             className=" text-black w-full h-[83px] rounded-lg bg-[#f6f6f6] flex items-center px-5 gap-4 font-bold cursor-pointer hover:bg-gray-200"
@@ -159,11 +160,15 @@ export default function MesAides() {
 
                             <div className="ml-auto">
                                 {userName && dossier.admin === userName && (
-                                <button type="button" className="text-[#f27474] hover:scale-110 transition-transform" onClick={() => setDossierToDelete(dossier)}>
+                                <button type="button" className="text-[#f27474] hover:scale-110 transition-transform" onClick={(e) => {
+                                    e.preventDefault(); // empêche la navigation
+                                    e.stopPropagation(); // empêche le click parent
+                                    setDossierToDelete(dossier);}}>
                                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
                                 </button>)}
                             </div>
                         </div>
+                            </Link>
                     )})}
                 </div>
             </div>
