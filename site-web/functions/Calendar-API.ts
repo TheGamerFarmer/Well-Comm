50d9ca1d43c385a4bffa45c19adec2659ba1215a
romain PLANQUETTE <romain.plqt@gmail.com>
2026-01-19 10:17:47 +0100

    correction URL backend

--- site-web/functions/Calendar-API.ts
+++ site-web/functions/Calendar-API.ts
@@ -1,16 +1,17 @@
 import FullCalendar from "@fullcalendar/react";
 import {RefObject} from "react";
+import { API_BASE_URL } from "@/config";
 
 export async function getEventsRequest(userName: string | null, activeRecordId: number | null, start: string, end: string) {
     return await fetch(
-        `http://localhost:8080/api/${userName}/records/${activeRecordId}/calendar/startDate/${start}/endDate/${end}`, {
+        `${API_BASE_URL}/api/${userName}/records/${activeRecordId}/calendar/startDate/${start}/endDate/${end}`, {
             credentials: 'include'
         }
     );
 }
 
 export async function saveEditsRequest(userName: string | null, activeRecordId: number | null, isCreating: boolean, editData: any, calendarRef: RefObject<FullCalendar | null>) {
-    const response = await fetch(`http://localhost:8080/api/${userName}/records/${activeRecordId}/calendar/event` + (isCreating ? "" : `/${editData.id}`), {
+    const response = await fetch(`${API_BASE_URL}/api/${userName}/records/${activeRecordId}/calendar/event` + (isCreating ? "" : `/${editData.id}`), {
         method: isCreating ? "POST" : "PUT",
         headers: {'Content-Type': 'application/json'},
         credentials: 'include',
@@ -22,7 +23,7 @@ export async function saveEditsRequest(userName: string | null, activeRecordId: 
 }
 
 export async function deleteEventRequest(userName: string | null, activeRecordId: number | null, eventId: string, calendarRef: RefObject<FullCalendar | null>) {
-    const response = await fetch(`http://localhost:8080/api/${userName}/records/${activeRecordId}/calendar/event/${eventId}`, {
+    const response = await fetch(`${API_BASE_URL}/api/${userName}/records/${activeRecordId}/calendar/event/${eventId}`, {
         method: "DELETE",
         credentials: 'include',
     });
