79d2d39f1b6d898ed6edb0cf20deb47ae38c7cfd
romain PLANQUETTE <romain.plqt@gmail.com>
2026-01-21 09:45:02 +0100

    config android

--- site-web/next.config.ts
+++ site-web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    //app android
+    /*
+    output: 'export',
+    trailingSlash: true,
+    basePath: '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+    distDir: '.next',
+     */
+
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
